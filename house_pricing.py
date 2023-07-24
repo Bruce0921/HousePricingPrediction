516f03feb37e68a7fcf5f12209bdd2d3d035c5cb
Bruce0921 <114181725+Bruce0921@users.noreply.github.com>
2023-07-24 13:04:49 -0700

    add a prediction example

--- house_pricing.py
+++ house_pricing.py
@@ -16,7 +16,7 @@ cat_cols = df.select_dtypes(include=['object']).columns
 
 # Check for missing values in numerical columns and fill them
 df[num_cols] = df[num_cols].fillna(df[num_cols].mean())
-print(df.columns)
+
 # Check for missing values in categorical columns and fill them
 for col in cat_cols:
     df[col].fillna(df[col].mode()[0], inplace=True)
@@ -34,7 +34,6 @@ sns.scatterplot(x = df['median_house_value'], y =df['median_income'])
 plt.title('Median Income vs Median House Value')
 plt.show()
 
-
 # Apply one-hot encoding to 'ocean_proximity'
 df = pd.get_dummies(df, columns=['ocean_proximity'], prefix="", prefix_sep="")
 
@@ -49,9 +48,11 @@ df_scaled = pd.DataFrame(scaler.fit_transform(df_features), columns=df_features.
 # Append target back to the DataFrame
 df_scaled = pd.concat([df_scaled, target.reset_index(drop=True)], axis=1)
 
-# Splitting the Data
+# Splitting the data
 X = df_scaled.drop('median_house_value', axis=1)
 y = df_scaled['median_house_value']
+
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a Model
@@ -92,3 +93,46 @@ plt.title('Median Income vs House Value')
 plt.legend()
 
 plt.show()
+
+# Assuming this is your new data (make sure it has the same structure as your training data)
+new_data = {
+    'longitude': [-122.23],
+    'latitude': [37.88],
+    'housing_median_age': [41.0],
+    'total_rooms': [880.0],
+    'total_bedrooms': [129.0],
+    'population': [322.0],
+    'households': [126.0],
+    'median_income': [8.3252],
+    'ocean_proximity': ['NEAR BAY']
+}
+
+# Convert the dictionary into a DataFrame
+new_data_df = pd.DataFrame(new_data)
+
+# One-hot encoding for 'ocean_proximity'
+new_data_df = pd.get_dummies(new_data_df, columns=['ocean_proximity'], prefix="", prefix_sep="")
+
+# Ensure new data has all the columns that the model was trained on
+missing_cols = set(df_features.columns) - set(new_data_df.columns)
+for c in missing_cols:
+    new_data_df[c] = 0
+
+# Ensure the order of column in the test query is in the same order than in train set
+new_data_df = new_data_df[df_features.columns]
+
+# Ensure all column names are strings
+new_data_df.columns = new_data_df.columns.astype(str)
+
+# Standardizing the features
+new_features_scaled = scaler.transform(new_data_df)
+
+# Convert scaled features to DataFrame and ensure it has correct column names
+new_features_df = pd.DataFrame(new_features_scaled, columns=df_features.columns)
+
+# Now you can use the trained model to make predictions on your new data
+new_prediction = model.predict(new_features_df)
+
+print("Predicted house value: ", new_prediction)
+
+
